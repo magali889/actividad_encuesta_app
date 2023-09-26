43b09c103943e0eecd02cf5e8fc2ed80f27b1d18
magali889 <maguiii889@gmail.com>
2023-09-26 01:13:35 -0300

    actividad_encuesta_app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Encuestas from './components/Encuestas';
+
+
+
+
 
 function App() {
+
+  const encuestas = [
+    { id: 1, 
+      pregunta: '¿Cuál es tu color favorito?', 
+      opciones: ['Rojo', 'Azul', 'Verde'] },
+
+
+    { id: 2, 
+      pregunta: '¿Cuál es tu comida favorita?', 
+      opciones: ['Pizza', 'Hamburguesa', 'Sushi'] },
+    ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div className="App">
+       <h1>Aplicación de Encuestas</h1>
+
+       <Encuestas encuestas={encuestas} />
+     </div>
+   );
 }
+
 
 export default App;
---
+++ src/components/Encuestas.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+
+function Encuestas({ encuestas }) {
+     
+      return (
+      <div>
+          <h2>Encuestas Disponibles</h2>
+          <ul>
+             {encuestas.map(encuesta => (
+            <li key={encuesta.id}>{encuesta.pregunta}</li>
+             ))}
+          </ul>
+     </div> 
+     );
+}
+export default Encuestas;
+
